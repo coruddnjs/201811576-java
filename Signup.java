fcc7a46037cabdcb970ba0c930e32cec9b14cf1d
coruddnjs <77574576+coruddnjs@users.noreply.github.com>
2022-04-01 19:07:35 +0900

    Update Signup.java

--- Signup.java
+++ Signup.java
@@ -6,18 +6,20 @@ import javax.swing.*;
 
 public class Signup extends JFrame{
 	String license[] = {"1종","2종","대형"};
+	String com[] = {"SKT","KT","LGU+"};
 	int size=100;
 	
 	JComboBox<String> cb = new JComboBox<String> (license);
-	
+	JComboBox<String> co = new JComboBox<String> (com);
+ 	
 	Container c = getContentPane();
 	
-	JLabel name = new JLabel("이름");
-	JLabel ID_t = new JLabel("ID ");
-	JLabel PW_t = new JLabel("PW ");
-	JLabel Int = new JLabel("생년월일");
-	JLabel Phone = new JLabel("휴대번호");
-	JLabel cb_n = new JLabel("운전면허유무");
+	JLabel name = new JLabel("이름: ");
+	JLabel ID_t = new JLabel("ID: ");
+	JLabel PW_t = new JLabel("PW: ");
+	JLabel Int = new JLabel("생년월일: ");
+	JLabel Phone = new JLabel("휴대번호: ");
+	JLabel cb_n = new JLabel("운전면허유무: ");
 	JLabel das = new JLabel("-");
 	JLabel das1 = new JLabel("-");
 	
@@ -25,8 +27,8 @@ public class Signup extends JFrame{
 	JButton Sign = new JButton("가입하기");
 	
 	JTextField ID_d = new JTextField(10);
-	JTextField PW_d = new JTextField(10);
-	JTextField name_d = new JTextField(10);
+	JPasswordField PW_d = new JPasswordField(10);
+	JTextField name_d = new JTextField(4);
 	JTextField Int_d = new JTextField(6);
 	JTextField P1 = new JTextField(3);
 	JTextField P2 = new JTextField(4);
@@ -41,7 +43,7 @@ public class Signup extends JFrame{
 		name.setSize(size,20);
 		name.setHorizontalAlignment(JLabel.LEFT);
 		
-		name_d.setLocation(50,10);
+		name_d.setLocation(60,10);
 		name_d.setSize(size,20);
 		name_d.setHorizontalAlignment(JLabel.LEFT);
 		
@@ -49,7 +51,7 @@ public class Signup extends JFrame{
 		ID_t.setSize(size,20);
 		ID_t.setHorizontalAlignment(JLabel.LEFT);
 		
-		ID_d.setLocation(50,50);
+		ID_d.setLocation(60,50);
 		ID_d.setSize(size,20);
 		ID_d.setHorizontalAlignment(JLabel.LEFT);
 		
@@ -57,15 +59,16 @@ public class Signup extends JFrame{
 		PW_t.setSize(size,20);
 		PW_t.setHorizontalAlignment(JLabel.LEFT);
 		
-		PW_d.setLocation(50,90);
+		PW_d.setLocation(60,90);
 		PW_d.setSize(size,20);
 		PW_d.setHorizontalAlignment(JLabel.LEFT);
+		PW_d.setEchoChar('*');
 		
 		Int.setLocation(0,130);
 		Int.setSize(size,20);
 		PW_t.setHorizontalAlignment(JLabel.LEFT);
 		
-		Int_d.setLocation(50,130);
+		Int_d.setLocation(60,130);
 		Int_d.setSize(size,20);
 		PW_d.setHorizontalAlignment(JLabel.LEFT);
 		
@@ -81,19 +84,19 @@ public class Signup extends JFrame{
 		Phone.setSize(size,20);
 		Phone.setHorizontalAlignment(JLabel.LEFT);
 		
-		P1.setLocation(60, 200);
-		P2.setLocation(110, 200);
-		P3.setLocation(180, 200);
+		P1.setLocation(130, 200);
+		P2.setLocation(175, 200);
+		P3.setLocation(240, 200);
 		P1.setSize(30,20);
 		P2.setSize(50,20);
 		P3.setSize(50,20);
 		P1.setHorizontalAlignment(JLabel.LEFT);
 		P2.setHorizontalAlignment(JLabel.LEFT);
 		P3.setHorizontalAlignment(JLabel.LEFT);
-		das.setLocation(95, 200);
+		das.setLocation(165, 200);
 		das.setSize(50,20);
 		das.setHorizontalAlignment(JLabel.LEFT);
-		das1.setLocation(165, 200);
+		das1.setLocation(230, 200);
 		das1.setSize(50,20);
 		das1.setHorizontalAlignment(JLabel.LEFT);
 		
@@ -101,13 +104,16 @@ public class Signup extends JFrame{
 		cb_n.setLocation(0, 170);
 		cb_n.setSize(size,20);
 		cb_n.setHorizontalAlignment(JLabel.LEFT);
+		co.setBounds(60, 200, 60, 20);
+		
 		
 		Back.addActionListener(new Backs());
 		
+		c.add(co);
 		c.add(das);
 		c.add(das1);
 		c.add(cb_n);
-		c.add(cb);
+		c.add(cb); 
 		c.add(Phone);
 		c.add(P1);
 		c.add(P2);
@@ -124,10 +130,10 @@ public class Signup extends JFrame{
 		c.add(Back);
 		c.add(Sign);
 		
-		setSize(300, 350);
+		setSize(330, 350);
 		setVisible(true);
 		
-			}
+	}
 		class Backs implements ActionListener{
 			@Override
 			public void actionPerformed(ActionEvent e) {
