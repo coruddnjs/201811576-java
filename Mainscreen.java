70c880a7ca8b9a9a0fe55c9f82f32423b4da97ab
coruddnjs <77574576+coruddnjs@users.noreply.github.com>
2022-04-16 16:07:23 +0900

    Update Mainscreen.java

--- Mainscreen.java
+++ Mainscreen.java
@@ -3,59 +3,79 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Mainscreen {
-	public static void main(String[] args) {
-		Screen sc = new Screen();
-	}
-}
-class Screen extends JFrame{
+class Mainscreen extends JFrame{
 	Container c = getContentPane();
+	int size=90;
 	
-	JPanel p1 = new JPanel();
-	JPanel p2 = new JPanel();
-	
-	ImageIcon img = new ImageIcon(new ImageIcon("C:\\jsp-work\\test\\src\\image\\Porsche.jpg")
-			.getImage().getScaledInstance(150, 150, Image.SCALE_DEFAULT));
-	ImageIcon img1 = new ImageIcon(new ImageIcon("C:\\jsp-work\\test\\src\\image\\benz.jpg")
-			.getImage().getScaledInstance(150, 150, Image.SCALE_DEFAULT));
-	
-	JButton b1 = new JButton("포르쉐",img);
-	JButton b2 = new JButton("벤츠",img1);
+	JButton b1 = new JButton("차량 예약");
+	JButton b2 = new JButton("예약 확인");
+	JButton b3 = new JButton("예약 취소");
+	JButton b4 = new JButton("차량 검색");
 	
 	
-	public Screen() {
+	
+	public Mainscreen() {
 		setTitle("welcome"); 
-		setSize(500,500);
+		setSize(300,250);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		c.setLayout(new BorderLayout());
-		p1.setBackground(Color.WHITE);
-		p1.setLayout(null);
-		p2.setBackground(Color.WHITE);
-		p2.setLayout(null);
+		c.setLayout(null);
 		
-		p1.setSize(150,150);
-		p2.setSize(150,150);
+		b1.setLocation(100,10);
+		b1.setSize(size,20);
+		b1.setHorizontalAlignment(JButton.LEFT);
+		c.add(b1);
 		
-		b1.addActionListener(new Button());
-		b1.setLocation(0, 10);
-		b1.setSize(150,150);
+		b2.setLocation(100,50);
+		b2.setSize(size,20);
+		b2.setHorizontalAlignment(JButton.LEFT);
+		c.add(b2);
 		
-		b2.setLocation(170,10);
-		b2.setSize(150,150);
+		b3.setLocation(100,90);
+		b3.setSize(size,20);
+		b3.setHorizontalAlignment(JButton.LEFT);
+		c.add(b3);
 		
-		p1.add(b1);
-		p2.add(b2);
-		c.add(p1);
-		c.add(p2);
+		b4.setLocation(100,130);
+		b4.setSize(size,20);
+		b4.setHorizontalAlignment(JButton.LEFT);
+		c.add(b4);
 		
+		b1.addActionListener(new Button1());
+		b2.addActionListener(new Button2());
+		b3.addActionListener(new Button3());
+		b4.addActionListener(new Button4());
 		setVisible(true);
 		
 	}
-	class Button implements ActionListener{
+	class Button1 implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
 			String car1 = e.getActionCommand();
-			if(car1.equals("포르쉐")) {
-				Signup s = new Signup();
+			if(car1.equals("차량 예약")) {
+				Signup a = new Signup();
+			}
+		}
+	}
+	class Button2 implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			String car1 = e.getActionCommand();
+			if(car1.equals("예약 확인")) {
+				Signup a = new Signup();
+			}
+		}
+	}
+	class Button3 implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			String car1 = e.getActionCommand();
+			if(car1.equals("예약 취소")) {
+				Signup a = new Signup();
+			}
+		}
+	}
+	class Button4 implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			String car1 = e.getActionCommand();
+			if(car1.equals("차량 검색")) {
+				Signup a = new Signup();
 			}
 		}
 	}
